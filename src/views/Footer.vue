35cadd330da2e2be885bbf125f73a3014fd668eb
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-12 01:43:55 +0430

    add footers inside tags and classes

---
+++ src/views/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="footer">
+      <div class="footer__row">
+          <ul class="footer__links">
+              <li class="footer__item"><a href="" class="footer__link">test</a></li>
+              <li class="footer__item"><a href="" class="footer__link">test</a></li>
+              <li class="footer__item"><a href="" class="footer__link">test</a></li>
+              <li class="footer__item"><a href="" class="footer__link">test</a></li>
+              <li class="footer__item"><a href="" class="footer__link">test</a></li>
+          </ul>
+          <ul class="footer__social">
+              <li class="footer__item"><a href="" class="footer__link">test</a></li>
+              <li class="footer__item"><a href="" class="footer__link">test</a></li>
+              <li class="footer__item"><a href="" class="footer__link">test</a></li>
+          </ul>
+          <ul class="footer__links">
+              <li class="footer__item"><a href="" class="footer__link">test</a></li>
+              <li class="footer__item"><a href="" class="footer__link">test</a></li>
+              <li class="footer__item"><a href="" class="footer__link">test</a></li>
+              <li class="footer__item"><a href="" class="footer__link">test</a></li>
+          </ul>
+      </div>
+      <p class="footer__copyright">copyright</p>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
