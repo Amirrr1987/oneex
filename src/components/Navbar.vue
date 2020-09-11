06e84b508bbf23922f606b8432d4c99885846997
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-11 14:42:37 +0430

    add nav ul li a & classes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,1 +1,17 @@
-vue
+<template>
+  <nav class="navbar">
+      <ul class="nav">
+          <li class="navbar__item"><a href="" class="navbar__link">home</a></li>
+      </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+}
+</script>
+
+<style>
+
+</style>
