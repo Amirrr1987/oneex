bc60716b7ddb72b6c64c0d1054416c05df0f6e41
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-11 14:00:56 +0430

    add header file tags and imp navbar create navbar

---
+++ src/views/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+      <Navbar/>
+  </header>
+</template>
+
+<script>
+import moduleName from '@/components/Navbar.vue';
+export default {
+    name: 'Header',
+    components:{
+        Navbar,
+    }
+}
+</script>
+
+<style>
+
+</style>
