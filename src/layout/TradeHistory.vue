b163fd86d7bbff556bd58a2b1acd1ac08eecfee0
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-12 16:03:09 +0430

    create trade hsitory and BEM class add

---
+++ src/layout/TradeHistory.vue
@@ -0,0 +1,49 @@
+<template>
+  <main class="main trade-history">
+      <section class="trade-history__header">
+          <h3 class="trade-history__title">trade history</h3>
+          <div class="trade-history__head">trade-history</div>
+      </section>
+      <section class="trade-history__body">
+          <table class="trade-history__table">
+                <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+          </table>
+      </section>
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'TradeHistory',
+}
+</script>
+
+<style>
+
+</style>
