c38fc1775c8d44131f92f0bee66b8af3fb5244a7
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-12 13:38:30 +0430

    shaodw border active and add to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,9 @@
   <nav class="navbar">
       <a href="" class="navbar__logo-link"><img src="@/assets/img/logo.svg" title="oneex" alt="oneex" class="navbar__logo-img"></a>
       <ul class="navbar__nav">
-          <li class="navbar__item"><a href="" class="navbar__link">home</a></li>
-          <li class="navbar__item"><a href="" class="navbar__link">home</a></li>
-          <li class="navbar__item"><a href="" class="navbar__link">home</a></li>
+          <li class="navbar__item"><a href="" class="navbar__link">exchange</a></li>
+          <li class="navbar__item"><a href="" class="navbar__link">transaction</a></li>
+          <li class="navbar__item"><a href="" class="navbar__link">transaction</a></li>
           <li class="navbar__item"><a href="" class="navbar__link">home</a></li>
       </ul>
       <ul class="navbar__nav">
