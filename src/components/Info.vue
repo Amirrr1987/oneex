268262454bb428ee05276d34e33aea2d43c24161
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-12 15:53:31 +0430

    fix class info

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,5 +1,27 @@
 <template>
-  <section class="info">info</section>
+  <section class="info">
+      <h4 class="info__item">TIC / BTC</h4>
+      <div class="info__item">
+          <h5 class="info__title">24hr Volume</h5>
+          <span class="">5.89000891BTC</span>
+      </div>
+      <div class="info__item">
+          <h5 class="info__title">24hr Volume</h5>
+          <span class="">5.89000891BTC</span>
+      </div>
+      <div class="info__item">
+          <h5 class="info__title">24hr Volume</h5>
+          <span class="">5.89000891BTC</span>
+      </div>
+      <div class="info__item">
+          <h5 class="info__title">24hr Volume</h5>
+          <span class="">5.89000891BTC</span>
+      </div>
+      <div class="info__item">
+          <h5 class="info__title">24hr Volume</h5>
+          <span class="">5.89000891BTC</span>
+      </div>
+  </section>
 </template>
 
 <script>
